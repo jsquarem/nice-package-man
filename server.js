1ca97c0fc75e01457224a5da21b0f5610573d1a4
jmartymar <jeffjmart@gmail.com>
2022-08-18 14:25:23 -0500

    fix multiple user profile reference

--- server.js
+++ server.js
@@ -75,16 +75,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const getProfile = async (id) => {
-  const [profile] = await Profile.find({ profileId: id }).exec();
+  const [profile] = await Profile.find({ googleId: id }).exec();
   return profile;
 };
 
 // Add this middleware BELOW passport middleware
 app.use(async function (req, res, next) {
   res.locals.user = req.user;
-  if (typeof req.user != "undefined") {
-    res.locals.user = await getProfile(req.user._id);
-  }
+  if (typeof req.user != "undefined")
+    res.locals.user = await getProfile(req.user.googleId);
   //res.locals.user = req.user; // assinging a property to res.locals, makes that said property (user) availiable in every
   // single ejs view
   next();
